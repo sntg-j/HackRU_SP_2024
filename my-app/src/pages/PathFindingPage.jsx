1b5f0706b7b327a82f8ccea4eb790e163715d89d
Medhasriv <medhasri.vel@gmail.com>
2024-03-23 14:14:49 -0400

    Committing all JSX pages

--- my-app/src/pages/PathFindingPage.jsx
+++ my-app/src/pages/PathFindingPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to My App</h1>
-      <p>This is the home page of my React application.</p>
+      <Navbar />
+      <h1>ROUTE PAGE</h1>
     </div>
   );
 }
