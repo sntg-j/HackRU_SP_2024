91b0d9297a6bacc716a5b6e209baf276f6546dbd
Medhasriv <medhasri.vel@gmail.com>
2024-03-23 18:11:32 -0400

    Pushing initial home page + extra files

---
+++ my-app/src/pages/RecipeReaderPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/PathFindingPage.jsx
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function RecipeReaderPage() {
+  return (
+    <div>
+      <h1>RECIPE READING PAGE</h1>
+    </div>
+  );
+}
+
+export default RecipeReaderPage;
