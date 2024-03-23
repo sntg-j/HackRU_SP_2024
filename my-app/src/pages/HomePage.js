c783801226826b9aed4f57ed31231d79427ae4a4
Medhasriv <medhasri.vel@gmail.com>
2024-03-23 13:47:27 -0400

    Initalizing the pages for the app

---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is the home page of my React application.</p>
+    </div>
+  );
+}
+
+export default HomePage;
