133ae5b6b2a60c1d7d550e6b68b324218151a333
Medhasriv <medhasri.vel@gmail.com>
2024-03-23 18:11:32 -0400

    Pushing initial home page + extra files

---
+++ my-app/src/pages/PathFindingPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/PathFindingPage.jsx
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function PathFindingPage() {
+  return (
+    <div>
+      <h1>ROUTE PAGE</h1>
+    </div>
+  );
+}
+
+export default PathFindingPage;
